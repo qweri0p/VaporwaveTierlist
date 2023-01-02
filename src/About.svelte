0dd0815273302ebb12195e38ff7a4a5b98b1c1b6
qweri0p <71005650+qweri0p@users.noreply.github.com>
2023-01-02 22:36:23 +0100

    oops

--- src/About.svelte
+++ src/About.svelte
@@ -1,5 +1,5 @@
 <a target="_blank" rel="noopener noreferrer" href="https://github.com/qweri0p/VaporwaveTierlist">
-    <img src="/github.svg" alt="Github link">
+    <img src="./github.svg" alt="Github link">
 </a>
 
 <style>
